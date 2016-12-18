735129af6ffe7f4ee4ba07dc4788252f6c37c163
Damon McMinn <dmcminn@vccp.com>
2016-12-18 22:14:10 +0000

    Initial test changes
    - Remove Checkout.com tests
    - Skeleton DrinkServiceTests

---
+++ Checkout.ApiClient.Tests/DrinkService/DrinkServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using FluentAssertions;
+using NUnit.Framework;
+
+using Checkout.ApiServices.Drinks.RequestModels;
+using Checkout.ApiServices.Drinks.ResponseModels;
+
+namespace Tests
+{
+    public class DrinkServiceTests : BaseServiceTests
+    {
+
+    }
+}
